29ff3f3ed6c895b628d174430595dca2e9a7b8db
Sian Culligan <SianCulligan@gmail.com>
2020-02-19 21:31:49 -0800

    rendering json to page

--- hp-houses.js
+++ hp-houses.js
@@ -5,29 +5,34 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const data = require('./hphouse.json')
+
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
 
+//SAVE - USE AS REFERENCE, THIS WILL TRAVERSE THE OBJECT
+// app.get('/', (request, response) => {
+//   response.send(data[0].rival);
+// });
+
 app.get('/', (request, response) => {
-  response.send('/hphouse.json');
-});
+    response.send(data);
+  });
 
 //helper functions (error catching)
 function errorHandler(error, request, response) {
   response.status(500).send(error);
 }
 
-function startServer() {
-    app.listen(process.env.PORT, () => console.log(`Server running on ${PORT}`));
-  }
-  
-  startServer();
-  
+//server "listener"
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
+  });
 
 
